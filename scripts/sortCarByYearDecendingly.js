9a64cedd058ec304753cddf219687dfb1ac81634
fitriandriy <202410102062@mail.unej.ac.id>
2024-04-19 20:31:03 +0700

    fix: remove built in functions

--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -1,14 +1,18 @@
 function sortCarByYearDescendingly(cars) {
-  // Sangat dianjurkan untuk console.log semua hal hehe
   console.log(cars);
 
-  // Clone array untuk menghindari side-effect
-  // Apa itu side effect?
-  const result = [...cars];
+  const arr = [...cars];
 
-  // Tulis code-mu disini
-  result.sort((a, b) => b.year - a.year)
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j].year < arr[j + 1].year) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
 
-  // Rubah code ini dengan array hasil sorting secara descending
-  return result;
+  return arr;
 }
